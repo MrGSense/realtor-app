9fddb899dc233e6e167aa50fb716346b6eaaf5bb
Kyle Fetterhoff <kyle.fetterhoff@gmail.com>
2021-03-20 14:35:24 -0400

    Add initial pages templates

---
+++ pages/views.py
@@ -0,0 +1,10 @@
+from django.http.response import HttpResponsePermanentRedirect
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'pages/index.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
