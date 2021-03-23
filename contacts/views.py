4843f45d3c37b1421e0f0bbd5815da9220f27b4b
Kyle Fetterhoff <kyle.fetterhoff@gmail.com>
2021-03-22 23:17:07 -0400

    Remove email to realtor

--- contacts/views.py
+++ contacts/views.py
@@ -32,15 +32,15 @@ def contact(request):
 
         contact.save()
 
-        # Send email to realtor of listing
-        send_mail(
-            'Property Listing Inquiry',
-            'There has been an inquiry for ' + listing +
-            '. Sign into the admin panel for more info.',
-            'kyle.fetterhoff@gmail.com',
-            [realtor_email, 'kyle.fetterhoff@gmail.com'],
-            fail_silently=False
-        )
+        # # Send email to realtor of listing
+        # send_mail(
+        #     'Property Listing Inquiry',
+        #     'There has been an inquiry for ' + listing +
+        #     '. Sign into the admin panel for more info.',
+        #     'kyle.fetterhoff@gmail.com',
+        #     [realtor_email, 'kyle.fetterhoff@gmail.com'],
+        #     fail_silently=False
+        # )
 
         messages.success(
             request, 'Your request has been submitted, a realtor will get back to you soon')
